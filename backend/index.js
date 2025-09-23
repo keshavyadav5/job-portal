b5cdc8d9b62a250a81fc492b7ea18e6913cf3fa7
keshayadav5 <kesavyadav992@gmail.com>
2025-09-23 13:59:10 +0530

    user model |  controller | route

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+const app = express()
+import dotenv from 'dotenv'
+import connectDb from './utils/db.js'
+import userRoute from './routes/user.route.js'
+dotenv.config({})
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true
+}
+app.use(cors(corsOption))
+const PORT = process.env.PORT || 3000;
+
+
+// apis
+app.use('/api/v1/user', userRoute)
+app.listen(PORT, () => {
+  console.log(`server running at port ${PORT}`)
+  connectDb()
+})
