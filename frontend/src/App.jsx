0949452740594544b14ac91ac4ecfd8fe006e425
keshayadav5 <kesavyadav992@gmail.com>
2025-09-24 00:36:47 +0530

    initialize frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+    <Button className='text-2xl'>ehasfsf</Button>
+    </div>
+  )
+}
+
+export default App
